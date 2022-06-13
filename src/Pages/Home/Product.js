bbcf08f3a7e9989d7c2809f2c3d26eaedd951898
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-06-13 19:19:46 +0600

    update product card

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -2,35 +2,84 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
-  const { _id, title, description, price, stock, minOrderQty, img } = product;
+  const { _id, title, description, price, stock, img } = product;
 
   return (
-    <div
-      className="card lg:max-w-lg bg-base-100 border shadow-xl
-    hover:border-t-4 hover:border-primary
-    "
-    >
-      <figure>
-        <img style={{ height: '220px' }} src={img} alt="Shoes" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p className="text-success font-semibold">Price: {price}$</p>
-        <p>{description?.slice(0, 48)}...</p>
-        <p>
-          <span className="text-success font-semibold">Available stock:</span>{' '}
-          {stock} Pieces
-        </p>
-        <small className="text-error">Min Order: {minOrderQty} Pieces</small>
+    <div class="max-w-sm mx-auto overflow-hidden bg-white rounded-lg drop-shadow-md dark:bg-gray-800">
+      <img
+        class="object-cover object-center w-full h-56"
+        src={img}
+        alt={`${title}`}
+      />
+      <div class="px-6 py-4">
+        <h1 class="text-xl font-semibold text-gray-800 dark:text-white">
+          {title}
+        </h1>
 
-        <div className="card-actions justify-center">
-          <Link
-            to={`/purchase/${_id}`}
-            className="mx-auto text-primary text-xl"
+        <p class="py-1 text-gray-700 dark:text-gray-400">
+          {description?.slice(0, 80)} ...
+        </p>
+
+        <div class="flex items-center mt-2 text-gray-700 dark:text-gray-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
           >
-            Buy Now
-          </Link>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+
+          <h1 class="mx-1 mr-16 text-lg font-semibold dark:text-gray-400 text-gray-800">
+            {price}
+          </h1>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M3 12v3c0 1.657 3.134 3 7 3s7-1.343 7-3v-3c0 1.657-3.134 3-7 3s-7-1.343-7-3z" />
+            <path d="M3 7v3c0 1.657 3.134 3 7 3s7-1.343 7-3V7c0 1.657-3.134 3-7 3S3 8.657 3 7z" />
+            <path d="M17 5c0 1.657-3.134 3-7 3S3 6.657 3 5s3.134-3 7-3 7 1.343 7 3z" />
+          </svg>
+          <h1 class="mx-1 text-lg font-semibold dark:text-gray-400 text-gray-800">
+            {stock}
+          </h1>
         </div>
+
+        <Link
+          to={`/purchase/${_id}`}
+          class="flex items-center px-6 py-1 mt-3 bg-gray-900"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-shopping-cart"
+            width="26"
+            height="26"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#ffffff"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="6" cy="19" r="2" />
+            <circle cx="17" cy="19" r="2" />
+            <path d="M17 17h-11v-14h-2" />
+            <path d="M6 5l14 1l-1 7h-13" />
+          </svg>
+
+          <h1 class="mx-3 text-lg font-semibold text-white">BUY NOW</h1>
+        </Link>
       </div>
     </div>
   );
--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -14,15 +14,17 @@ const Products = () => {
   }
 
   return (
-    <div className="mx-12 mb-16">
+    <div>
       <div className="mb-8 text-center">
         <h1 className="text-3xl font-semibold m-4">PRODUCT RECOMMENDATION</h1>
         <div className="mx-auto w-24 bg-primary p-0.5 bg-gradient-to-r from-purple-600 to-primary"></div>
       </div>
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-12">
-        {products.slice(0, 6).map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
+      <div class="container px-6 py-8 mx-auto">
+        <div class="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {products.slice(0, 8).map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
