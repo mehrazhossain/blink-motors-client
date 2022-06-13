a9570cf037cef4c8d8896886123df108628241e0
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-06-13 14:17:03 +0600

    remove opacity from banner

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -8,21 +8,21 @@ const Banner = () => {
       <img
         className="absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-4/5 opacity-25 sm:opacity-100"
         src={banner}
-        alt="Couple on a bed with a dog"
+        alt="Couple on a bike"
       />
 
-      <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white sm:to-transparent"></div>
+      {/* <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white sm:to-transparent"></div> */}
 
       <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
         <div className="max-w-xl text-center sm:text-left">
-          <h1 className="text-3xl font-extrabold sm:text-5xl uppercase">
+          <h1 className="text-3xl font-extrabold sm:text-5xl uppercase text-white">
             Motorcycle Parts
             <strong className="font-extrabold text-primary sm:block">
               AND ACCESSORIES
             </strong>
           </h1>
 
-          <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl">
+          <p className="max-w-lg mt-4 sm:leading-relaxed sm:text-xl text-white">
             With the increasing demand in the motorcycle industry in the
             Bangladesh, we make sure to keep the quality of all our items. They
             are tested and durable to support the safety of our customers.
